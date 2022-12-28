a16372729699e2a80641c40d3c9094bedd0f9e4c
Brian McAllister <brian.mcallister@lab49.com>
2022-12-28 11:49:36 -0500

    Fix eslint config options, use legacy config.

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  env: {
+    jest: true,
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "airbnb-typescript",
+    "plugin:eslint-comments/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:storybook/recommended",
+  ],
+  rules: {
+    "prettier/prettier": "error",
+    "import/prefer-default-export": "off",
+    "import/no-default-export": "error",
+    "@typescript-eslint/quotes": "off",
+    "@typescript-eslint/comma-dangle": "off",
+  },
+  overrides: [
+    {
+      files: ["./stories/**/*stories*"],
+      rules: {
+        "import/no-default-export": 0,
+      },
+    },
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
